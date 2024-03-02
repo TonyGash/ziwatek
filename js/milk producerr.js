66f06f2a95d85314e3a7ece1fe8cfbfcfbee3867
Antony Sydney Ratemo <81922580+RatemoSydney@users.noreply.github.com>
2024-03-02 12:22:46 +0300

    Delete js/milk producerr.js

--- js/milk producerr.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('milk-produce-link').addEventListener('click', function(event) {
-  event.preventDefault();
-
-  fetch('milk_produce.html')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('main-content').innerHTML = data;
-    });
-});
